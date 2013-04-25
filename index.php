f628ff5155cab6dd2f25a898ef2c61cc1931ea5f
Simonarde Jr <simonarde@gmail.com>
2013-04-25 08:13:37 -0300

    Commit das bibliotecas CSS, JavaScript e index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!--
+
+	Copyright 2011 - Victor Antunes Vieira, Márcio Guedes Cavalcante e 
+	
+	Simonarde Lima e Silva Júnior sob orientação do Prof. Luiz Augusto Matos
+	
+	da Silva com apoio da Universidade Federal do Acre - UFAC.
+	
+	
+	Copyright 2013 - Simonarde Lima e Silva Júnior
+	
+	
+	
+	Este arquivo é parte do programa Kaxinawa - Sistema para Gestão de Editais
+
+
+	Kaxinawa - Sistema para Gestão de Editais é um software livre; você pode 
+    
+	redistribui-lo e/ou modifica-lo dentro dos termos da Licença Pública Geral 
+    
+	GNU como publicada pela Fundação do Software Livre (FSF); na versão 2 da 
+
+	Licença, ou (na sua opnião) qualquer versão.
+
+
+
+	Este programa é distribuido na esperança que possa ser  util, 
+
+	mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer
+
+	MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+
+	Licença Pública Geral GNU para maiores detalhes.
+
+
+
+	Você deve ter recebido uma cópia da Licença Pública Geral GNU
+
+	junto com este programa, se não, escreva para a Fundação do Software
+
+	Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+-->
+
+<?php ?>
+<!doctype html>
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Kaxinawa - Sistema para Gestão de Editais</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
